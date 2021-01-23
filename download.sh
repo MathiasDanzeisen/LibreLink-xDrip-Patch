e2217222551a3eea1bf62ff908528941e8bf70ac
MathiasDanzeisen <matze@specialo.de>
2021-01-23 21:49:34 +0100

    adapted download.sh to version v2.4.2

--- download.sh
+++ download.sh
@@ -8,14 +8,14 @@ WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
 
 WORKDIR=$(pwd)
-FILENAME='com.freestylelibre.app.de_2019-04-22'
+FILENAME='com.freestylelibre.app.de_2020-05-19' # version v2.4.2
 
 # wget HSTS-bugfix for debian as subsystem in Windows
 touch ~/.wget-hsts
 chmod 644 ~/.wget-hsts
 
 echo -e "${WHITE}Lade original APK herunter ...${NORMAL}"
-wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/4751-APK
+wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/5960-APK
 URL=$(grep "hier klicken" APK/apkpure.html | sed 's#^.*https://##' | sed 's/">.*//')
 wget -O APK/${FILENAME}.apk --load-cookies cookies.txt https://${URL}
 if [ $? = 0 ]; then
@@ -35,7 +35,7 @@ echo "Info: Debian liefert eine nicht ohne weiteres funktionierende 'dirty'-Vers
 mkdir -p tools
 wget -q -O tools/apktool https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
 chmod 755 tools/apktool
-wget -q -O tools/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.0.jar
+wget -O tools/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.0.jar
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
   echo
